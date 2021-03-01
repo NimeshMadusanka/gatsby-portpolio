ecbe846f00489bf15409cb54da1fe319e810a4ec
Nimesh <42901200+NimeshMadusanka@users.noreply.github.com>
2021-03-02 02:22:41 +0530

    update site

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: 'I am Nimesh Madusanka who completed SE specialized degree at SLIIT. I used JavaScript, Typescript, Java, Python, Dart, and PHP programming languages. I lead my Research team when did the Foodzy Research Project. So, I have worked under pressure, handle changes and improve good communicational skills. I like to learn new things and quickly adapt to the working environment.',
   paragraphTwo: 'I have more than two years of experience in business development process and freelancing projects. I have done an 8th-month intern in Wijeya Digital Media and more than two years of freelancing experience at Apex IT Solutions. At the moment, I have handled industrial, Research, Assessment, and Documentary projects under Web, Mobile, and Desktop Applications. In the project section, a few of them are added.',
   paragraphThree: 'If you want to download my resume, please click the bellow button',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1Blob2YLtQZ7_AA4kMSjLaB7hpCctbJEL/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -42,7 +42,7 @@ export const projectsData = [
     info: 'Healthy Food Network (Foodzy) is a research project in which users can actively contribute to health promotions. This is a mobile application users can share their consumer food images. Users get healthy suggestions, instructions, and information.',
     info2: 'Used technologies are Python, CNN, TensorFlow, Keras, Scikit learn , React Native front end with expo, Firebase Database, and Heroku algorithm APIs',
     url: 'https://expo.io/@nimesh_madusanka/projects/foodzy',
-    repo: 'https://github.com/NimeshMadusanka', // if no repo, the button will not show up
+    repo: 'https://github.com/NimeshMadusanka/foodzy-research', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -51,16 +51,16 @@ export const projectsData = [
     info: 'The quiz website is a learning platform for computer science. It provides multiple-choice questions with answers and scores.  Users can learn new things quickly throw the quiz.',
     info2: 'Used technologies are React framework, Typescript, CSS, opentdb API',
     url: 'http://computer-science-quiz.cf/',
-    repo: 'https://github.com/NimeshMadusanka', // if no repo, the button will not show up
+    repo: 'https://github.com/NimeshMadusanka/cs-quiz', // if no repo, the button will not show up
   },
     {
     id: nanoid(),
-    img: 'project4.jpg',
+    img: 'project4.png',
     title: 'Q4C Shop',
     info: 'Q4C Shop is an online shopping site which user can buy their preferred product. Users have to provide shipping details before doing the payments.',
     info2: 'Used technologies are React framework, React redux, Email.js, Google login, JSON Webtoken, Java Script, Bootstrap 4, Cloudinary, Paypal getway, Rest API back end, Express JS, Mongo DB Database',
     url: 'http://computer-science-quiz.cf/',
-    repo: 'https://github.com/NimeshMadusanka', // if no repo, the button will not show up
+    repo: 'https://github.com/NimeshMadusanka/q4c-shop', // if no repo, the button will not show up
   },
     {
     id: nanoid(),
@@ -69,7 +69,7 @@ export const projectsData = [
     info: 'Q4C News AI is a news reading platform using Alen AI. It provides the latest Daily news. Users can search and read throw the voice commands.',
     info2: 'Used technologies are React framework, React redux, Email.js, Google login, JSON Webtoken, Java Script, Bootstrap 4, Cloudinary, Paypal getway, Rest API back end, Express JS, Mongo DB Database',
     url: 'https://q4c-news-ai.ga',
-    repo: 'https://github.com/NimeshMadusanka', // if no repo, the button will not show up
+    repo: 'https://github.com/NimeshMadusanka/q4c-news-ai', // if no repo, the button will not show up
   },
     {
     id: nanoid(),
@@ -77,8 +77,8 @@ export const projectsData = [
     title: 'Technical Blog site',
     info: 'This is the blog site in which I share my knowledge with the IT community. I discuss more trending topics related to full-stack developers in 2021.',
     info2: 'Used technologies are Typescript, Next.js, react, SCSS, Ghost backend',
-    url: 'http://computer-science-quiz.cf/',
-    repo: 'https://github.com/NimeshMadusanka', // if no repo, the button will not show up
+    url: 'https://technical-blog.ga/',
+    repo: 'https://github.com/NimeshMadusanka/technical-blog', // if no repo, the button will not show up
   },
     {
     id: nanoid(),
@@ -103,23 +103,23 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
+      name: 'web',
+      url: 'apex-solutions.ga',
     },
     {
       id: nanoid(),
       name: 'facebook',
-      url: '',
+      url: 'https://www.facebook.com/Q4C-Solutions-102340038380957',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/nimesh-madusanka-605120169/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/NimeshMadusanka',
     },
   ],
 };
